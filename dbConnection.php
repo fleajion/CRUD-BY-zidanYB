6b7cd09489a84dce1ad177969b1df2772f31a95a
zidanYourBae <150268415+fleajion@users.noreply.github.com>
2025-03-12 23:13:26 +0700

    Create dbConnection.php

---
+++ dbConnection.php
@@ -0,0 +1,8 @@
+<?php
+$databaseHost = 'localhost';
+$databaseName = 'zidan69';
+$databaseUsername = 'root';
+$databasePassword = 'zidanGanteng123??';
+
+// Open a new connection to the MySQL server
+$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
